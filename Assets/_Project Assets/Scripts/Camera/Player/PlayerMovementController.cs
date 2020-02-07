b365178a9957bd59bc772b393f44f05aa31e7fa3
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-02-07 23:58:20 +0000

    Implement player movement

--- Assets/_Project Assets/Scripts/Camera/Player/PlayerMovementController.cs
+++ Assets/_Project Assets/Scripts/Camera/Player/PlayerMovementController.cs
@@ -1,45 +1,57 @@
 ﻿/*
  * PlayerMovementController.cs
- * Created by: #AUTHOR#
- * Created on: #CREATIONDATE# (dd/mm/yy)
- * Created for: #PURPOSE#
+ * Created by: Ambrosia
+ * Created on: 7/2/2020 (dd/mm/yy)
+ * Created for: controlling the movement of the Player
  */
 
 using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour
 {
-	// Uncomment these when there are variables in place
-	//[Header("Components")]
-	CharacterController _Controller;
-	Camera _MainCamera;
+    // Uncomment these when there are variables in place
+    //[Header("Components")]
+    CharacterController _Controller;
+    Camera _MainCamera;
 
-	[Header("Settings")]
-	[SerializeField] float _MovementSpeed = 3;
-	[SerializeField] float _GravityMultiplier = 1; 
-	
-	private void Awake()
-	{
-		_Controller = GetComponent<CharacterController>();
-		_MainCamera = Camera.main;
-	}
-	
-	private void Update()
-	{
-		HandleMovement();
-	}
+    [Header("Settings")]
+    [SerializeField] float _MovementSpeed = 3;
+    [SerializeField] float _Gravity = -Physics.gravity.y;
 
-	private void HandleMovement()
-	{
-		var mDirection = new Vector3(Input.GetAxis("Horizontal"),
-                                     0,
-                                     Input.GetAxis("Vertical")).normalized * _MovementSpeed;
+    private void Awake()
+    {
+        _Controller = GetComponent<CharacterController>();
+        _MainCamera = Camera.main;
+    }
 
-		if (mDirection.x == 0 && mDirection.z == 0)
-			return;
+    private void Update()
+    {
+        HandleMovement();
+    }
 
-		mDirection = _MainCamera.transform.TransformDirection(mDirection);
-		mDirection.y -= Physics.gravity.y * Time.deltaTime;
-		_Controller.Move(mDirection * Time.deltaTime);
-	}
+    private void HandleMovement()
+    {
+        // Get the Y velocity we're about to apply
+        float yVelocity = _Gravity * Time.deltaTime;
+        // Apply a downwards movement using the Y velocity
+        _Controller.SimpleMove(Vector3.down * yVelocity * Time.deltaTime);
+
+        // Get input from the 'Horizontal' and 'Vertical' axis, and normalize it to not let
+        // the player move quicker when going diagonally
+        var mDirection = new Vector3(Input.GetAxis("Horizontal"),
+                             0,
+                             Input.GetAxis("Vertical")).normalized;
+
+        // If the player has even touched the H and V axis
+        if (mDirection.x == 0 && mDirection.z == 0)
+            return;
+
+        // Make the movement vector relative to the camera's position/rotation
+        mDirection = _MainCamera.transform.TransformDirection(mDirection);
+        // Remove any Y momentum gained when doing the TransformDirection
+        mDirection.y = 0;
+
+        // Apply the movement using '_MovementSpeed' as a multiplier
+        _Controller.Move(mDirection * _MovementSpeed * Time.deltaTime);
+    }
 }
