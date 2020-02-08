fe9bd2c28d74fbdadadb0a9284adb00ff17aadd1
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-02-08 00:52:07 +0000

    Create Health interface

---
+++ Assets/_Project Assets/Scripts/Generics/IHealth.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * IHealth.cs
+ * Created by: Ambrosia
+ * Created on: 8/2/2020 (dd/mm/yy)
+ * Created for: requiring a general purpose interface to easily add to scripts
+ */
+
+public interface IHealth
+{
+    // Supposed to set the health
+    void SetHealth(int set);
+    // Supposed to take some health away
+    void TakeHealth(int take);
+    // Supposed to add some health
+    void GiveHealth(int give);
+
+    // Supposed to return the amount of health an object / entity has
+    int GetHealth();
+}
+
