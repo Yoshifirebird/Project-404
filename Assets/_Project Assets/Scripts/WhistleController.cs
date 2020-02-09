51653cfdc1c89993b9aceeeb4de902cbef5b5f04
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-02-08 22:12:54 -0500

    Added Visuals to whistle

--- Assets/_Project Assets/Scripts/WhistleController.cs
+++ Assets/_Project Assets/Scripts/WhistleController.cs
@@ -1,8 +1,8 @@
 ﻿/*
  * WhistleController.cs
- * Created by: Ambrosia
+ * Created by: Ambrosia & Kaelan
  * Created on: 9/2/2020 (dd/mm/yy)
- * Created for: controlling the whistle
+ * Created for: Controlling the whistle
  */
 
 using UnityEngine;
@@ -14,9 +14,21 @@ public class WhistleController : MonoBehaviour
 	[Header("Settings")]
 	[SerializeField] LayerMask _GroundLayer;
 	[SerializeField] float _YAxisOffset = 0.5f;
-	
+
+    [Header("Whistling")]
+    [SerializeField] [Range(0, 1)] float _WhistleRadiusGrowSpeed;
+    [SerializeField] [Range(0, 1)] float _WhistleHeightGrowSpeed;
+    [SerializeField] float _WhistleMaxTime;
+    float _CurrentWhistleTime;
+    [SerializeField] Vector3 _WhistleScale;
+    Vector3 _TargetScale;
+    Vector3 _StartingScale;
+    bool _IsWhistling;
+
 	private void Awake()
 	{
+        _StartingScale = transform.localScale;
+        _TargetScale = _StartingScale;
 		_Camera = Camera.main;
 	}
 	
@@ -30,9 +42,45 @@ public class WhistleController : MonoBehaviour
 			Vector3 position = hit.point;
 			position.y += _YAxisOffset;
 			transform.position = position;
-
-			// Rotate the whistle
-			transform.rotation = Quaternion.FromToRotation(Vector3.up, hit.normal);
 		}
+
+        HandleWhistle();
+
 	}
+
+    void HandleWhistle()
+    {
+        //Determines whether the player is whistling
+        if (Input.GetMouseButtonDown(1))
+        {
+            _IsWhistling = true;
+            _TargetScale = _WhistleScale;
+            _CurrentWhistleTime = 0;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            _IsWhistling = false;
+            _TargetScale = _StartingScale;
+            transform.localScale = _TargetScale;
+        }
+
+        //If the whistle's radius is almost maxed, start adding time
+        if(transform.localScale.x + 0.1f >= _WhistleScale.x)
+        {
+            _CurrentWhistleTime += Time.deltaTime;
+        }
+
+        if(_CurrentWhistleTime >= _WhistleMaxTime)
+        {
+            _IsWhistling = false;
+            _TargetScale = _StartingScale;
+            transform.localScale = _TargetScale;
+        }
+
+        transform.localScale = new Vector3(Mathf.Lerp(transform.localScale.x, _TargetScale.x, _WhistleRadiusGrowSpeed),
+            Mathf.Lerp(transform.localScale.y, _TargetScale.y, _WhistleHeightGrowSpeed),
+            Mathf.Lerp(transform.localScale.z, _TargetScale.z, _WhistleRadiusGrowSpeed));
+
+    }
+
 }
