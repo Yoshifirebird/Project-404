6bec99ee2886046632241e348f3a031794776a25
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-03-31 22:48:06 +0100

    :ambulance: :art: :sparkles: Completely revamp whistle system

---
+++ Assets/_Project Assets/Scripts/MathUtil.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public static class MathUtil
+{
+    public const float M_2PI = Mathf.PI * 2;
+
+    /// <summary>
+    /// Calculates the relative position of an object at an index in a circle
+    /// </summary>
+    /// <param name="segments">How many segments there are (quality)</param>
+    /// <param name="index">The index of the object to calculate the position of</param>
+    /// <returns>The relative position (-1 to 1, -1 to 1)</returns>
+    public static Vector2 CalcPosInCirc(uint segments, int index)
+    {
+        float theta = (M_2PI / segments) * index;
+        return new Vector2(Mathf.Cos(theta), Mathf.Sin(theta));
+    }
+
+    /// <summary>
+    /// Calculates the relative position of an object at an index in a circle with an added offset
+    /// </summary>
+    /// <param name="segments">How many segments there are (quality)</param>
+    /// <param name="index">The index of the object to calculate the position of</param>
+    /// <param name="offset">How much to offset the calculation by</param>
+    /// <returns>The relative position (-1 to 1, -1 to 1)</returns>
+    public static Vector2 CalcPosInCirc(uint segments, int index, float offset)
+    {
+        float theta = (M_2PI / segments) * index;
+        return new Vector2(Mathf.Cos(theta + offset), Mathf.Sin(theta + offset));
+    }
+}
--- Assets/_Project Assets/Scripts/Player/Whistle/WhistleController.cs
+++ Assets/_Project Assets/Scripts/Player/Whistle/WhistleController.cs
@@ -1,128 +1,198 @@
 ﻿/*
  * WhistleController.cs
  * Created by: Ambrosia & Kaelan
- * Created on: 9/2/2020 (dd/mm/yy)
+ * Created on: 9/2/2020, overhaul on 31/3/2020 (dd/mm/yy)
  * Created for: Controlling the whistle
  */
 
 using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
 
+[RequireComponent(typeof(AudioSource))]
 public class WhistleController : MonoBehaviour
 {
-    Camera _Camera;
+    /* We use the localScale to keep track of how scaled the Whistle
+     * actually is, as opposed to just changing the decals localScale
+     * 
+     */
 
     [Header("Components")]
-    [SerializeField] Transform _Player;
-    [SerializeField] Transform _Throw;
+    [SerializeField] ParticleSystem _WhistleParticle;
+    [SerializeField] AudioClip _BlowSound;
+
+    [Header("Particles")]
+    [SerializeField] uint _ParticleDensity = 15;
+    [SerializeField] float _ParticleRotationSpeed = 1;
+    [SerializeField] float _ParticleRaycastAddedHeight = 100;
+    [SerializeField] float _ParticleOffset = 0.5f;
 
     [Header("Settings")]
-    [SerializeField] LayerMask _GroundLayer;
-    [SerializeField] float _WhistleMaxDistance = Mathf.Infinity;
-    [SerializeField] float _ThrowMaxDistance = Mathf.Infinity;
-    [SerializeField] float _YAxisOffset = 0.5f;
+    [SerializeField] float _StartingRadius = 1;
+    [SerializeField] float _ExpandedRadius = 10;
+    [SerializeField] float _MaxBlowTime = 3;
 
-    [Header("Whistling")]
-    [SerializeField] Vector2 _WhistleScale;
-    [SerializeField] [Range(0, 1)] float _WhistleRadiusGrowSpeed;
-    [SerializeField] [Range(0, 1)] float _WhistleHeightGrowSpeed;
-    [SerializeField] float _WhistleMaxTime;
-    float _CurrentWhistleTime;
-    Vector3 _TargetScale;
-    Vector3 _StartingScale;
-    bool _IsWhistling;
+    [Header("Raycast Settings")]
+    [SerializeField] float _MaxDistance = Mathf.Infinity;
+    [SerializeField] LayerMask _MapMask;
 
-    AudioSource _AudioSource;
+    [Header("Debugging")]
+    [SerializeField] uint _WhistleCircleSegments = 20;
+
+    AudioSource _Source;
+    ParticleSystem[] _Particles;
+    Camera _MainCamera;
+
+    bool _Blowing = false;
+    float _TimeBlowing = 0;
 
     void Awake()
     {
-        _StartingScale = transform.localScale;
-        _TargetScale = _StartingScale;
-        _Camera = Camera.main;
-        _AudioSource = GetComponent<AudioSource>();
+        // Generate particles for blowing later on
+        _Particles = new ParticleSystem[_ParticleDensity + 1];
+        for (int i = 0; i < _Particles.Length; i++)
+        {
+            _Particles[i] = Instantiate(_WhistleParticle);
+        }
+        AssignParticlePositions();
+
+        // Reset the local scale
+        transform.localScale = Vector3.one * _StartingRadius;
+
+        // Get local components
+        _Source = GetComponent<AudioSource>();
+        _MainCamera = Camera.main;
     }
 
     void Update()
     {
-        // Shoot a raycast from the mouse position for an infinite distance, filtering out any
-        // hits other than objects that are on the _GroundLayer layer
-        if (Physics.Raycast(_Camera.ScreenPointToRay(Input.mousePosition), out RaycastHit hit, Mathf.Infinity, _GroundLayer.value, QueryTriggerInteraction.Ignore))
+        // Moving the whistle
+        Ray ray = _MainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, _MaxDistance, _MapMask, QueryTriggerInteraction.Ignore))
         {
-            Vector3 originalPosition = new Vector3(hit.point.x - _Player.position.x, (hit.point.y + _YAxisOffset) - _Player.position.y, hit.point.z -_Player.position.z);
-
-            transform.position = Vector3.ClampMagnitude(originalPosition, _WhistleMaxDistance) + _Player.position;
-            _Throw.transform.position = Vector3.ClampMagnitude(originalPosition, _ThrowMaxDistance) + _Player.position;
+            transform.position = hit.point;
         }
 
-        HandleWhistle();
-    }
-
-    void OnTriggerEnter(Collider other)
-    {
-        if (_IsWhistling)
-        {
-            CheckPikmin(other.gameObject);
-        }
-    }
-
-    void CheckPikmin(GameObject toCheck)
-    {
-        if (toCheck.layer == LayerMask.NameToLayer("Map"))
-            return;
-
-        var pikminComponent = toCheck.GetComponent<PikminBehavior>();
-        if (pikminComponent == null)
-            return;
-
-        pikminComponent.LatchOntoObject(null);
-        pikminComponent.AddToSquad();
-    }
-
-    void HandleWhistle()
-    {
-        //Determines whether the player is whistling
+        // Detecting Player input
         if (Input.GetButtonDown("Whistle"))
         {
-            _AudioSource.Play();
-            _IsWhistling = true;
-            SetVector3ToVector2(ref _TargetScale, _WhistleScale);
-            _CurrentWhistleTime = 0;
+            transform.localScale = Vector3.one * _StartingRadius;
+            _Blowing = true;
+            // Load the clip into the AudioSource and play
+            _Source.clip = _BlowSound;
+            _Source.Play();
+            // Start the particles
+            for (int i = 0; i < _Particles.Length; i++)
+            {
+                _Particles[i].Play();
+            }
         }
-        else if (Input.GetButtonUp("Whistle"))
+        if (Input.GetButtonUp("Whistle"))
         {
-            _AudioSource.Stop();
-            _IsWhistling = false;
-            _TargetScale = _StartingScale;
-            transform.localScale = _TargetScale;
+            EndBlow();
         }
-
-        //If the whistle's radius is almost maxed, start adding time
-        if (transform.localScale.x + 1.5f >= _WhistleScale.x)
+        // Handling Blowing
+        if (_Blowing)
         {
-            _CurrentWhistleTime += Time.deltaTime;
-            transform.localScale = new Vector3(transform.localScale.x,
-                                               Mathf.Lerp(transform.localScale.y, _TargetScale.y, _WhistleHeightGrowSpeed),
-                                               transform.localScale.z);
+            // Used to keep track of how long we've been blowing for
+            _TimeBlowing += Time.deltaTime;
+            if (_TimeBlowing >= _MaxBlowTime)
+            {
+                EndBlow();
+            }
+
+            // Grow the scale of the whistle to the radius we want it to become
+            float t = _TimeBlowing / _MaxBlowTime;
+            transform.localScale = Vector3.Lerp(transform.localScale, _2Dto3D(Vector2.one * _ExpandedRadius, 1), t);
+
+            Collider[] collisions = Physics.OverlapCapsule(transform.position + Vector3.down * 20, transform.position + Vector3.up * 20, transform.localScale.x);
+            for (int i = 0; i < collisions.Length; i++)
+            {
+                PikminBehavior pikComponent = collisions[i].GetComponent<PikminBehavior>();
+                if (pikComponent != null)
+                {
+                    pikComponent.AddToSquad();
+                }
+            }
         }
 
-        if (_CurrentWhistleTime >= _WhistleMaxTime)
-        {
-            _AudioSource.Stop();
-            _IsWhistling = false;
-            _TargetScale = _StartingScale;
-            transform.localScale = _TargetScale;
-        }
-
-        transform.localScale = new Vector3(Mathf.Lerp(transform.localScale.x, _TargetScale.x, _WhistleRadiusGrowSpeed),
-                                           transform.localScale.y,
-                                           Mathf.Lerp(transform.localScale.z, _TargetScale.z, _WhistleRadiusGrowSpeed));
-
+        // Handle Particle movement
+        AssignParticlePositions();
     }
 
-    void SetVector3ToVector2(ref Vector3 set, Vector2 to)
+    /// <summary>
+    /// Displays debug information about the Whistle when selected in the editor
+    /// </summary>
+    void OnDrawGizmosSelected()
     {
-        set.x = to.x;
-        set.z = to.x;
+        if (Application.isPlaying)
+        {
+            // Draw particles
+            AssignParticlePositions();
+            foreach (var particle in _Particles)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawSphere(particle.transform.position, 0.05f * transform.localScale.x);
+            }
+        }
 
-        set.y = to.y;
+        // Draw default whistle radius
+        for (int i = 0; i < _WhistleCircleSegments + 1; i++)
+        {
+            Vector3 pos = transform.position + _2Dto3D(MathUtil.CalcPosInCirc(_WhistleCircleSegments, i)) * _StartingRadius;
+            Gizmos.color = Color.yellow;
+            Gizmos.DrawSphere(pos, 0.05f * _StartingRadius);
+        }
+        // Draw expanded whistle radius
+        for (int i = 0; i < _WhistleCircleSegments + 1; i++)
+        {
+            Vector3 pos = transform.position + _2Dto3D(MathUtil.CalcPosInCirc(_WhistleCircleSegments, i)) * _ExpandedRadius;
+            Gizmos.color = Color.green;
+            Gizmos.DrawSphere(pos, 0.05f * _ExpandedRadius);
+        }
     }
+
+    void EndBlow()
+    {
+        transform.localScale = Vector3.one * _StartingRadius;
+        _TimeBlowing = 0;
+        _Blowing = false;
+        _Source.Stop();
+    }
+
+    /// <summary>
+    /// Assigns the positions of the blow particles
+    /// </summary>
+    void AssignParticlePositions()
+    {
+        for (int i = 0; i < _ParticleDensity + 1; i++)
+        {
+            Transform cacheTransform = transform;
+            Vector3 particlePos = cacheTransform.position + _2Dto3D(MathUtil.CalcPosInCirc(_ParticleDensity, i, _TimeBlowing * _ParticleRotationSpeed)) * cacheTransform.localScale.x;
+            // Cache the Y for later
+            float originalY = particlePos.y;
+
+            // Put the Y of the particle waay above everything else, so it can raycast downwards onto surfaces that may be above it 
+            particlePos.y += _ParticleRaycastAddedHeight;
+
+            // Check if there is a surface beneath the particle
+            if (Physics.Raycast(particlePos, Vector3.down, out RaycastHit hitInfo, _MaxDistance, _MapMask, QueryTriggerInteraction.Ignore))
+            {
+                particlePos.y = hitInfo.point.y + _ParticleOffset;
+            }
+            else
+            {
+                // We couldn't find anything, so reset back to the original Y
+                particlePos.y = originalY;
+            }
+
+            _Particles[i].transform.position = particlePos;
+        }
+    }
+
+    /// <summary>
+    /// Converts between 2D and 3D on the X and Z axis
+    /// </summary>
+    /// <param name="conv">The vector to convert</param>
+    /// <returns>Vector3 with X and Z set to the X and Y of the Vector2</returns>
+    Vector3 _2Dto3D(Vector2 conv, float y = 0) => new Vector3(conv.x, y, conv.y);
 }
