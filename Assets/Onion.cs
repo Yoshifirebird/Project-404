78369eee50e02cc7bcac9467944b78a51d91727b
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-04-12 03:44:40 +0100

    Create onion manager and onion script

---
+++ Assets/Onion.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Onion.cs
+ * Created by: Ambrosia
+ * Created on: 12/4/2020 (dd/mm/yy)
+ * Created for: #PURPOSE#
+ */
+
+using UnityEngine;
+
+public class Onion : MonoBehaviour
+{
+	//[Header("Components")]
+	
+	
+	//[Header("Settings")]
+	
+	
+	void Awake()
+	{
+		
+	}
+	
+	void Update()
+	{
+		
+	}
+	
+	void FixedUpdate()
+	{
+		
+	}
+}
---
+++ Assets/OnionManager.cs
@@ -0,0 +1,42 @@
+﻿/*
+ * OnionManager.cs
+ * Created by: Ambrosia
+ * Created on: 12/4/2020 (dd/mm/yy)
+ * Created for: needing a management system for the different onions
+ */
+
+using UnityEngine;
+
+public class OnionManager : MonoBehaviour
+{
+	//[Header("Components")]
+
+
+	//[Header("Settings")]
+
+	// This'll contain each onion type in an array the size of the Pikmin colours
+	// 0 for locked, 1 for unlocked
+	bool[] _UnlockedOnion;
+
+	public static OnionManager _Manager;
+
+	void Awake()
+	{
+		if (_Manager == null)
+			_Manager = this;
+		else
+			Destroy(gameObject);
+
+		_UnlockedOnion = new bool[(int)Colour.SIZE];
+		for (int i = 0; i < (int)Colour.SIZE; i++)
+		{
+			// Sets all of the booleans to false if they aren't the first (red onion)
+			_UnlockedOnion[i] = i == 0;
+		}
+	}
+	
+	public bool IsOnionUnlocked(Colour colour)
+	{
+		return _UnlockedOnion[(int)colour];
+	}
+}
