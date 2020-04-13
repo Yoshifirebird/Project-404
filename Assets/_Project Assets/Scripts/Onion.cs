e6b9a9670e9de0dddcbe994bca348838cb5c0687
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-04-13 11:28:59 +0100

    Imported some Onion sounds (thanks Pikhacker!), do some more onion script

--- Assets/_Project Assets/Scripts/Onion.cs
+++ Assets/_Project Assets/Scripts/Onion.cs
@@ -2,7 +2,7 @@
  * Onion.cs
  * Created by: Ambrosia
  * Created on: 12/4/2020 (dd/mm/yy)
- * Created for: #PURPOSE#
+ * Created for: Needing an object to store our Pikmin in
  */
 
 using UnityEngine;
@@ -10,23 +10,30 @@ using UnityEngine;
 public class Onion : MonoBehaviour
 {
 	//[Header("Components")]
-	
-	
-	//[Header("Settings")]
-	
-	
-	void Awake()
-	{
-		
-	}
-	
+
+
+	[Header("Settings")]
+	[SerializeField]
+	Colour _OnionColour;
+
+	PlayerStats.PikminStats _Stats;
+	bool _CanUse = false;
+
 	void Update()
 	{
-		
+		_Stats = PlayerStats.GetStats(_OnionColour);
+
+		/*if (_CanUse && Input.GetButtonDown())
+		{
+
+		}*/
 	}
-	
-	void FixedUpdate()
+
+	void OnTriggerEnter(Collider other)
 	{
-		
+		if (other.CompareTag("Player"))
+		{
+
+		}
 	}
 }
