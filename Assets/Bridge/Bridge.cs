8fb5c269cb1535f23290a8748e3de612cc2391e9
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-04-25 08:32:10 +0100

    Bridge works???

--- Assets/Bridge/Bridge.cs
+++ Assets/Bridge/Bridge.cs
@@ -14,23 +14,30 @@ public class Bridge : MonoBehaviour, IPikminAttack, IHealth
 	[SerializeField] Transform _Destination;
     [SerializeField] Transform _PileObject;
 
+    [Header("Prefabs")]
+    [SerializeField] GameObject _Endpoint;
+    [SerializeField] GameObject _Piece;
+
 	[Header("Settings")]
 	[SerializeField] float _HealthUntilStep = 10;
+    [SerializeField] float _HeightCorrection = 1;
 	float _CurrentHealth = 0;
 
-    // Step Variables
+    [Header("Stepping")]
     [SerializeField] float _StepDistance = 1;
-    int _CurrentStep = 0;
     int _StepsUntilFinish = 0;
 
 	List<PikminBehavior> _AttackingPikmin = new List<PikminBehavior>();
 
 	void Awake()
 	{
+        // Make the Pile look at the destination, so we can step towards it
+        _PileObject.LookAt(_Destination.position);
+
 		_CurrentHealth = _HealthUntilStep;
 
-        float distance = MathUtil.DistanceTo(transform.position, _Destination.position, false);
-        print(distance);
+        float distance = Mathf.Sqrt(MathUtil.DistanceTo(transform.position, _Destination.position, false));
+        _StepsUntilFinish = Mathf.CeilToInt(distance / _StepDistance);
 	}
 	
 	void Update()
@@ -45,7 +52,28 @@ public class Bridge : MonoBehaviour, IPikminAttack, IHealth
 
     void Step()
     {
+        _StepsUntilFinish--;
         
+        if (_StepsUntilFinish == 0)
+        {
+            Vector3 finalPos = Vector3.MoveTowards(_PileObject.position, _Destination.position, _StepDistance);
+            Quaternion finalRot = Quaternion.LookRotation(finalPos - _PileObject.position);
+
+            Instantiate(_Endpoint, finalPos, finalRot);
+
+            print("Arrived at destination :D");
+            // De-activate the bridge behaviour as it's been built
+            Destroy(_PileObject.gameObject);
+            Destroy(gameObject);
+        }
+
+        Vector3 nextPos = Vector3.MoveTowards(_PileObject.position, _Destination.position + Vector3.up * _HeightCorrection, _StepDistance);
+        Quaternion nextRot = Quaternion.LookRotation(nextPos - _PileObject.position);
+
+        nextRot = Quaternion.Euler(nextRot.eulerAngles.x - 26, nextRot.eulerAngles.y, nextRot.eulerAngles.z);
+        Instantiate(_Piece, _PileObject.position, nextRot);
+        _PileObject.rotation = nextRot;
+        _PileObject.position = nextPos;
     }
 
     #region Pikmin Attacking Implementation
