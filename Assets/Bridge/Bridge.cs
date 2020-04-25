d463b06fc8cbf52b5804f9fac675ee1492a5d959
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-04-25 06:40:06 +0100

    Import bridge fully, create materials, prefabs and fix problems with normals

---
+++ Assets/Bridge/Bridge.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Bridge.cs
+ * Created by: #AUTHOR#
+ * Created on: #CREATIONDATE# (dd/mm/yy)
+ * Created for: #PURPOSE#
+ */
+
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+	// Uncomment these when there are variables in place
+	//[Header("Components")]
+	
+	
+	//[Header("Settings")]
+	
+	
+	private void Awake()
+	{
+		
+	}
+	
+	private void Update()
+	{
+		
+	}
+}
