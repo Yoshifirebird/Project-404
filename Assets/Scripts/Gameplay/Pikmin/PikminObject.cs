82859b43d2163c1a7f3e6a3500a6e16b11f006c2
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-04-30 20:21:04 +0100

    Add Pikmin Scriptable objects and example 'Red' type

---
+++ Assets/Scripts/Gameplay/Pikmin/PikminObject.cs
@@ -0,0 +1,33 @@
+/*
+ * PikminObject.cs
+ * Created by: Ambrosia
+ * Created on: 30/4/2020 (dd/mm/yy)
+ */
+
+using UnityEngine;
+
+public enum PikminColour {
+  Red = 0,
+  Yellow,
+  Blue
+}
+
+public enum PikminMaturity {
+  Leaf = 0,
+  Bud,
+  Flower
+}
+
+[CreateAssetMenu (fileName = "NewPikminType", menuName = "New Pikmin Type")]
+public class PikminObject : ScriptableObject {
+  [Header ("Pikmin Specific")]
+  public PikminColour _Colour;
+  public PikminMaturity _Maturity;
+
+  [Header ("Movement")]
+  public float _MaxMovementSpeed;
+  public float _AccelerationSpeed;
+  public float _StoppingDistance;
+
+  public float _RotationSpeed;
+}
