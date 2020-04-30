2aa9cb8ccb69a2bfa06ec88b75a36ccad894520a
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-04-30 23:36:57 +0100

    Fix compiler error with IPikminAttack interface

--- Assets/Scripts/Interfaces/IPikminAttack.cs
+++ Assets/Scripts/Interfaces/IPikminAttack.cs
@@ -4,13 +4,12 @@
  * Created on: 30/4/2020 (dd/mm/yy)
  */
 
- public interface IPikminAttack
-{
-    // Called when the Pikmin starts attacking the object, like when latched
-    public void OnAttackStart(PikminAI pikmin);
-    // Called when the Pikmin changes states, or the object dies
-    public void OnAttackEnd(PikminAI pikmin);
+public interface IPikminAttack {
+  // Called when the Pikmin starts attacking the object, like when latched
+  void OnAttackStart (PikminAI pikmin);
+  // Called when the Pikmin changes states, or the object dies
+  void OnAttackEnd (PikminAI pikmin);
 
-    // Called when the Pikmin hits the object
-    public void OnAttackRecieve();
+  // Called when the Pikmin hits the object
+  void OnAttackRecieve ();
 }
