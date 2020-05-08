fb9755bd6a9e63a63040630d5e3ce12d7429e811
Newgame+ LD <sirchirzalot@gmail.com>
2020-05-08 13:22:58 -0700

    Mixed feelings about the water...

---
+++ Assets/_External Assets/Water/Optimized Water/Script/StandardWater.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandardWater : MonoBehaviour {
+
+    public Vector2 currentSpeed;
+    public Vector2 altCurrentSpeed;
+
+    private float offsetX;
+    private float offsetY;
+
+    private float altOffsetX;
+    private float altOffsetY;
+    private Renderer renderer;
+
+    private void Start()
+    {
+        renderer = GetComponent<Renderer>();
+    }
+
+    void Update () {
+        offsetX += Time.deltaTime * currentSpeed.x;
+        offsetY += Time.deltaTime * currentSpeed.y;
+
+        altOffsetX += Time.deltaTime * altCurrentSpeed.x;
+        altOffsetY += Time.deltaTime * altCurrentSpeed.y;
+        renderer.material.mainTextureOffset = new Vector2(offsetX, offsetY);
+        renderer.material.SetTextureOffset("_DetailAlbedoMap", new Vector2(altOffsetX, altOffsetY));
+	}
+}
