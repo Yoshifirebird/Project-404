74331561d13b0e5289f97727c9bd5794df4cb549
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-05-23 23:34:21 +0100

    Create DayController

---
+++ Assets/Scripts/Gameplay/DayController.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * DayController.cs
+ * Created by: Ambrosia
+ * Created on: 23/5/2020 (dd/mm/yy)
+ */
+
+using UnityEngine;
+
+public class DayController : MonoBehaviour
+{
+  [Header("Debugging")]
+  [SerializeField] DayState _State = DayState.Morning;
+  [SerializeField] float _CurrentTime = 0;
+
+  void Update()
+  {
+    _CurrentTime += Time.deltaTime;
+  }
+
+  #region Public Functions
+
+  public float GetCurrentTime()
+  {
+    return _CurrentTime;
+  }
+}
