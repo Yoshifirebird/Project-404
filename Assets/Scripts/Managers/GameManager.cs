d3c7969d5202b5f6620d4e0dc670dab820ffda9e
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-05-23 22:45:35 +0100

    Add language option to GameManager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -4,9 +4,17 @@
  * Created on: 30/4/2020 (dd/mm/yy)
  */
 
+public enum Language
+{ 
+  English,
+  French,
+}
+
 public static class GameManager {
   public static bool _IsPaused = false; // Used in checks to see if the game is paused
   public static bool _FunMode = false; // Used in checks when you want to mess around, enables odd features
+
+  public static Language _Language = Language.English; // Used for alternate texts
 
   public static Player _Player = null; // Static reference to the Player in the current scene
 }
