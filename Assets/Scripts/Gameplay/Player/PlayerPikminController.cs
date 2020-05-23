9625598dfd748ee7958822ada614c9f2a6bf57fe
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-05-24 00:12:16 +0100

    Add visualisation for Pikmin stats

--- Assets/Scripts/Gameplay/Player/PlayerPikminController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerPikminController.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * PlayerPikminController.cs
  * Created by: Ambrosia
  * Created on: 23/5/2020 (dd/mm/yy)
@@ -6,24 +6,22 @@
 
 using UnityEngine;
 
-public class PlayerPikminController : MonoBehaviour
-{
+public class PlayerPikminController : MonoBehaviour {
 
-  void Update()
-  {
-    if (Input.GetButtonDown("X Button"))
-    {
-      while (PikminStatsManager._InSquad.Count > 0)
-      {
+  void Update () {
+    if (Input.GetButtonDown ("X Button")) {
+      while (PikminStatsManager._InSquad.Count > 0) {
         var pikminInSquad = PikminStatsManager._InSquad[0];
-        pikminInSquad.GetComponent<PikminAI>().RemoveFromSquad();
+        pikminInSquad.GetComponent<PikminAI> ().RemoveFromSquad ();
       }
     }
   }
 
-  void OnGUI()
-  {
-    Rect currentPosition = new Rect(50, 50, 50, 50);
-    GUI.Label(currentPosition, "hello");
+  void OnGUI () {
+    if (GameManager._DebugGui) {
+      GUI.Label (new Rect (10, 10, 300, 500), PikminStatsManager._RedStats.ToString ());
+      GUI.Label (new Rect (10, 80, 300, 500), PikminStatsManager._YellowStats.ToString ());
+      GUI.Label (new Rect (10, 150, 300, 500), PikminStatsManager._BlueStats.ToString ());
+    }
   }
 }
