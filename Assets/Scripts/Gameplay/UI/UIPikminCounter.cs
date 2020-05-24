4b51f2c68aff903c66c5e31cb890aefed32f2c71
Newgame+ LD <sirchirzalot@gmail.com>
2020-05-24 16:00:44 -0700

    Added animation to the Pikmin Counter. Now it looks even more fancy!

--- Assets/Scripts/Gameplay/UI/UIPikminCounter.cs
+++ Assets/Scripts/Gameplay/UI/UIPikminCounter.cs
@@ -4,13 +4,33 @@ using UnityEngine.UI;
 public class UIPikminCounter : MonoBehaviour {
   [SerializeField] Text _OnFieldText;
   [SerializeField] Text _InSquadText;
+	[SerializeField] ColorLerper flasher;
 
   void Update () {
-    _OnFieldText.text = (PikminStatsManager.GetOnField (PikminColour.Red) +
-      PikminStatsManager.GetOnField (PikminColour.Yellow) +
-      PikminStatsManager.GetOnField (PikminColour.Blue)).ToString ();
-    _InSquadText.text = (PikminStatsManager.GetInSquad (PikminColour.Red) +
-      PikminStatsManager.GetInSquad (PikminColour.Yellow) +
-      PikminStatsManager.GetInSquad (PikminColour.Blue)).ToString ();
+
+		int areaValue = (PikminStatsManager.GetOnField (PikminColour.Red) +
+			PikminStatsManager.GetOnField (PikminColour.Yellow) +
+			PikminStatsManager.GetOnField (PikminColour.Blue));
+		int squadValue = (PikminStatsManager.GetInSquad (PikminColour.Red) +
+			PikminStatsManager.GetInSquad (PikminColour.Yellow) +
+			PikminStatsManager.GetInSquad (PikminColour.Blue));
+
+			if(_OnFieldText.text != areaValue.ToString ())
+			{
+				_OnFieldText.gameObject.SetActive(false);	//Setting them inactive then active to make animation play
+				_OnFieldText.text = areaValue.ToString ();
+
+				_OnFieldText.gameObject.SetActive(true);
+			}
+
+			if(_InSquadText.text != squadValue.ToString ())
+			{
+				_InSquadText.gameObject.SetActive(false);
+				_InSquadText.text = squadValue.ToString ();
+
+				_InSquadText.gameObject.SetActive(true);
+			}
+
+		flasher.index = areaValue >= 100 ? 1 : 0;
   }
 }
