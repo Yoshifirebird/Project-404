f07d24ab839d8865825231863d46a0c64072314b
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-05-25 12:33:27 +0100

    Implement getting the closest Pikmin to the player

--- Assets/Scripts/Gameplay/Player/PlayerPikminController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerPikminController.cs
@@ -8,18 +8,57 @@ using UnityEngine;
 
 public class PlayerPikminController : MonoBehaviour {
 
+  [Header("Throwing")]
+  [SerializeField] float _PikminGrabRadius = 5;
+  [SerializeField] float _MaxGrabHeight = 1;
+
   void Update () {
     if (Input.GetButtonDown ("X Button")) {
       PikminStatsManager.ClearSquad ();
+    }
+    if (Input.GetKeyDown(KeyCode.Alpha4))
+    {
+      print(GetClosestPikmin());
     }
   }
 
   void OnGUI () {
     if (GameManager._DebugGui) {
-      int yOffset = 200;
-      GUI.Label (new Rect (10, yOffset + 10, 300, 500), PikminStatsManager._RedStats.ToString ());
-      GUI.Label (new Rect (10, yOffset + 80, 300, 500), PikminStatsManager._YellowStats.ToString ());
-      GUI.Label (new Rect (10, yOffset + 150, 300, 500), PikminStatsManager._BlueStats.ToString ());
+      int yOffset = 210;
+      GUI.Label (new Rect (10, yOffset, 300, 500), PikminStatsManager._RedStats.ToString ());
+      GUI.Label (new Rect (10, yOffset + 70, 300, 500), PikminStatsManager._YellowStats.ToString ());
+      GUI.Label (new Rect (10, yOffset + 140, 300, 500), PikminStatsManager._BlueStats.ToString ());
     }
+  }
+
+  GameObject GetClosestPikmin()
+  {
+    // C = closest, Pik = Pikmin, Dist = Distance
+    GameObject cPik = null;
+    float cPikDist = _PikminGrabRadius;
+
+    foreach (var pikmin in PikminStatsManager._InSquad)
+    {
+      // Check the distance between the Pikmin and the Player
+      float pikDist = MathUtil.DistanceTo(transform.position, pikmin.transform.position, false);
+      if (pikDist >= _PikminGrabRadius * _PikminGrabRadius)
+      {
+        continue;
+      }
+
+      float yDist = Mathf.Abs(transform.position.y - pikmin.transform.position.y);
+      if (yDist < _MaxGrabHeight)
+      {
+        continue;
+      }
+
+      if (pikDist < cPikDist)
+      {
+        cPikDist = pikDist;
+        cPik = pikmin;
+      }
+    }
+
+    return cPik;
   }
 }
