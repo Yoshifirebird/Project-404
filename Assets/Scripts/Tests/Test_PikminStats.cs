76a5f287de75817afcfaf696494c761976ac0df1
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2020-05-24 20:25:48 -0400

    Updated Test_PikminStats to spawn in pikmin instead of adding nonexistant ones

--- Assets/Scripts/Tests/Test_PikminStats.cs
+++ Assets/Scripts/Tests/Test_PikminStats.cs
@@ -1,5 +1,5 @@
 /*
- * NewBehaviourScript.cs
+ * Test_PikminStats.cs
  * Created by: Ambrosia
  * Created on: 30/4/2020 (dd/mm/yy)
  */
@@ -10,21 +10,11 @@ public class Test_PikminStats : MonoBehaviour {
   [Header ("Settings")]
   [SerializeField] KeyCode _KeyToApply = KeyCode.Alpha1;
   [SerializeField] KeyCode _KeyToPrint = KeyCode.Alpha0;
-
-  [Header ("Modifiers")]
-  [SerializeField] PikminMaturity _Maturity = default;
-  [SerializeField] PikminColour _Colour = default;
-  [SerializeField] PikminStatSpecifier _Specifier = default;
-  [SerializeField] bool _Add = true;
+  [SerializeField] GameObject _Pikmin;
 
   void Update () {
     if (Input.GetKeyDown (_KeyToApply)) {
-      if (_Add) {
-        PikminStatsManager.Add (_Colour, _Maturity, _Specifier);
-      }
-      else {
-        PikminStatsManager.Remove (_Colour, _Maturity, _Specifier);
-      }
+      Instantiate(_Pikmin, transform.position, Quaternion.identity);
     }
 
     if (Input.GetKeyDown (_KeyToPrint)) {
