26de3e01907ccfb65316177bf3f773435ada7f07
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-05-25 10:46:53 +0100

    Implement Health interface for the Player

--- Assets/Scripts/Gameplay/Player/Player.cs
+++ Assets/Scripts/Gameplay/Player/Player.cs
@@ -6,10 +6,19 @@
 
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : MonoBehaviour, IHealth {
+  [Header ("Components")]
   public Transform _FormationCentre = null;
 
+  [Header ("Settings")]
+  [SerializeField] float _MaxHealth = 100;
+
+  [Header ("Debugging")]
+  [SerializeField] float _CurrentHealth = 0;
+
   void Awake () {
+    _CurrentHealth = _MaxHealth;
+
     // Apply singleton pattern to allow for the objects in the scene to reference the active Player instance
     if (GameManager._Player == null) {
       GameManager._Player = this;
@@ -26,4 +35,30 @@ public class Player : MonoBehaviour {
       Time.timeScale = GameManager._IsPaused ? 0 : 1;
     }
   }
+
+  #region Health Implementation
+
+  public float GetCurrentHealth () {
+    return _CurrentHealth;
+  }
+
+  public float GetMaxHealth () {
+    return _MaxHealth;
+  }
+
+  public void SetHealth (float h) {
+    _CurrentHealth = h;
+  }
+
+  public float AddHealth (float h) {
+    _CurrentHealth += h;
+    return _CurrentHealth;
+  }
+
+  public float SubtractHealth (float h) {
+    _CurrentHealth -= h;
+    return h;
+  }
+
+  #endregion
 }
