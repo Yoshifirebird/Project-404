8bf4799a90b0c65924cb5871239d429f18bc0d26
JohnFiddleystein <patrickantonisawesome@gmail.com>
2020-06-18 15:17:24 +0100

    Format PPC, start to add functionality to throwing

--- Assets/Scripts/Gameplay/Player/PlayerPikminController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerPikminController.cs
@@ -6,97 +6,96 @@
 
 using UnityEngine;
 
-public class PlayerPikminController : MonoBehaviour
-{
-
-  [Header("Throwing")]
+public class PlayerPikminController : MonoBehaviour {
+  [Header ("Throwing")]
   [SerializeField] float _PikminGrabRadius = 5;
   [SerializeField] float _MaxGrabHeight = 1;
+  [SerializeField] Transform _ReticleTransform = null;
 
-  [Header("Debug")]
+  [Header ("Debug")]
   [SerializeField] int _GrabFXQuality = 50;
   [SerializeField] GameObject _HoldingPikmin = null;
+  PikminAI _HoldingPikminAI = null;
 
-  void Update()
-  {
-    if (Input.GetButtonDown("X Button"))
-    {
-      PikminStatsManager.ClearSquad();
+  void Update () {
+    if (Input.GetButtonDown ("X Button")) {
+      PikminStatsManager.ClearSquad ();
     }
 
     bool attemptedGrab = false;
 
-    if (Input.GetButtonDown("A Button"))
-    {
+    if (Input.GetButtonDown ("A Button")) {
       attemptedGrab = true;
-      if (PikminStatsManager.GetTotalInSquad() > 0)
-      {
-        _HoldingPikmin = GetClosestPikmin();
+      if (PikminStatsManager.GetTotalInSquad () > 0) {
+        _HoldingPikmin = GetClosestPikmin ();
+        if (_HoldingPikmin != null) {
+          _HoldingPikminAI = _HoldingPikmin.GetComponent<PikminAI> ();
+          _HoldingPikminAI.StartThrowHold ();
+        }
       }
     }
 
-    if (_HoldingPikmin != null)
-    {
+    if (_HoldingPikmin != null) {
       // TODO: Add holding and throwing
-      print("Holding pikmin");
+      if (Input.GetButton ("A Button")) {
+        _HoldingPikmin.transform.position = transform.position + transform.forward;
+      }
+      else {
+        // Throw the Pikmin!
+        _HoldingPikminAI.EndThrowHold (_ReticleTransform.position);
+
+        _HoldingPikmin = null;
+        _HoldingPikminAI = null;
+      }
     }
-    else if (attemptedGrab)
-    {
+    else if (attemptedGrab) {
       // We couldn't grab a Pikmin, we can punch now
 
-      print("Starting punch");
+      print ("Starting punch");
     }
   }
 
-  void OnGUI()
-  {
-    if (GameManager._DebugGui)
-    {
+  void OnGUI () {
+    if (GameManager._DebugGui) {
       int yOffset = 210;
-      GUI.Label(new Rect(10, yOffset, 300, 500), PikminStatsManager._RedStats.ToString());
-      GUI.Label(new Rect(10, yOffset + 70, 300, 500), PikminStatsManager._YellowStats.ToString());
-      GUI.Label(new Rect(10, yOffset + 140, 300, 500), PikminStatsManager._BlueStats.ToString());
+      GUI.Label (new Rect (10, yOffset, 300, 500), PikminStatsManager._RedStats.ToString ());
+      GUI.Label (new Rect (10, yOffset + 70, 300, 500), PikminStatsManager._YellowStats.ToString ());
+      GUI.Label (new Rect (10, yOffset + 140, 300, 500), PikminStatsManager._BlueStats.ToString ());
     }
   }
 
-  void OnDrawGizmosSelected()
-  {
+  void OnDrawGizmosSelected () {
     Gizmos.color = Color.green;
-    for (int i = 0; i < _GrabFXQuality; i++)
-    {
-      Vector3 endPosition = transform.position + MathUtil.XZToXYZ(_PikminGrabRadius * MathUtil.PositionInUnit(_GrabFXQuality, i));
-      
+    for (int i = 0; i < _GrabFXQuality; i++) {
+      Vector3 endPosition = transform.position + MathUtil.XZToXYZ (_PikminGrabRadius * MathUtil.PositionInUnit (_GrabFXQuality, i));
+
       // Visualise the upper bounds of the grab height limitation
-      Gizmos.DrawLine(endPosition, endPosition + (Vector3.up * _MaxGrabHeight));
-      Gizmos.DrawLine(endPosition + (Vector3.up * _MaxGrabHeight), transform.position);
+      Gizmos.DrawLine (endPosition, endPosition + (Vector3.up * _MaxGrabHeight));
+      Gizmos.DrawLine (endPosition + (Vector3.up * _MaxGrabHeight), transform.position);
 
       // Visualise the lower bounds of the grab height limitation
-      Gizmos.DrawLine(endPosition, endPosition + (Vector3.down * _MaxGrabHeight));
-      Gizmos.DrawLine(endPosition + (Vector3.down * _MaxGrabHeight), transform.position);
+      Gizmos.DrawLine (endPosition, endPosition + (Vector3.down * _MaxGrabHeight));
+      Gizmos.DrawLine (endPosition + (Vector3.down * _MaxGrabHeight), transform.position);
     }
   }
 
-  GameObject GetClosestPikmin()
-  {
+  GameObject GetClosestPikmin () {
     // C = closest, Pik = Pikmin, Dist = Distance
     GameObject cPik = null;
     float cPikDist = _PikminGrabRadius;
 
-    foreach (var pikmin in PikminStatsManager._InSquad)
-    {
+    foreach (var pikmin in PikminStatsManager._InSquad) {
       // Check the height difference between the Pikmin and the Player
-      float yDist = Mathf.Abs(transform.position.y - pikmin.transform.position.y);
+      float yDist = Mathf.Abs (transform.position.y - pikmin.transform.position.y);
 
-      if (yDist >= _MaxGrabHeight)
-      {
+      if (yDist >= _MaxGrabHeight) {
         continue;
       }
 
       // Check the distance between the Pikmin and the Player
-      float pikDist = MathUtil.DistanceTo(transform.position, pikmin.transform.position, false);
+      float pikDist = MathUtil.DistanceTo (transform.position, pikmin.transform.position, false);
 
-      if (pikDist < cPikDist)
-      {
+      if (pikDist < cPikDist) {
         cPikDist = pikDist;
         cPik = pikmin;
       }
